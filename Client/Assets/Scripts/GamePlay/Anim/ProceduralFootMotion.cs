fede51246307aabf153a78dbcd13d51dd1e559f8
K23 <13377236133@qq.com>
2025-04-23 21:29:46 +0800

    蜘蛛机器人功能迭代

--- Client/Assets/Scripts/GamePlay/Anim/ProceduralFootMotion.cs
+++ Client/Assets/Scripts/GamePlay/Anim/ProceduralFootMotion.cs
@@ -2,16 +2,33 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
+public enum ERelationWithBody 
+{
+    Left,
+    Right,
+
+}
+
 public class ProceduralFootMotion : MonoBehaviour
 {
+    public ERelationWithBody relationWithBody;
     public Transform bodyRoot;
     public ProceduralMoveController bodyController;
+
     public Vector3 offsetWithRoot;
-    public float maxDistanceWithNextFootHold;
-    public float stepHeight;
-    public float stepDistance;
-    public float stepTime;
-    public float stepAnimHeight;
+    public float feetMaxDistanceWithNextStep => bodyController.feetMaxDistanceWithNextStep;
+    public float stepHeight => bodyController.stepHeight;
+    public float stepDistance => bodyController.stepDistance;
+
+    public float stepAngle => bodyController.stepAngle;
+    public float nextStepCheckRadius => bodyController.nextStepCheckRadius;
+    public float stepTime
+    {
+        get {return (feetMaxDistanceWithNextStep + stepDistance)/(bodyController.moveSpeed*2f);}
+    }
+            
+    public float stepAnimHeight => bodyController.stepAnimHeight;
 
     public float height => m_curFootHold.y;
     
@@ -32,6 +49,11 @@ public class ProceduralFootMotion : MonoBehaviour
     
     void Update()
     {
+        // CheckNextStepGround();
+
+        Debug.DrawLine(m_hitInfo.point, m_hitInfo.point + Vector3.up,Color.black);
+        // Debug.DrawLine(m_curFootHold, m_curFootHold + Vector3.up, Color.yellow);
+        // Debug.DrawLine(m_nextFootHold, m_nextFootHold + Vector3.up, Color.green);
         if (m_move)
         {
             m_movePassTime += Time.deltaTime;
@@ -43,7 +65,7 @@ public class ProceduralFootMotion : MonoBehaviour
             }
             else
             {
-                m_footPosition.y = Mathf.Lerp(m_curFootHold.y + stepAnimHeight, m_nextFootHold.y, (value - 0.5f) * 2);
+                m_footPosition.y = Mathf.Lerp(m_curFootHold.y + stepAnimHeight, m_nextFootHold.y, (value - 0.5f) * 1.5f);
             } 
 
             transform.position = m_footPosition;
@@ -55,44 +77,107 @@ public class ProceduralFootMotion : MonoBehaviour
         }
     }
 
+    public bool CheckCanMove()
+    {
+        CheckNextStepGround();
+        return m_nextStepGrounded;
+    }
+
     public void TryMove(bool check)
     {
         if (!check)
             return;
 
-        CheckGround();
-        if (!m_move && m_nextStepGrounded)
-        { 
-            m_nextFootHold = m_hitInfo.point + bodyController.direction * stepDistance;
-            if (Vector3.Distance(m_curFootHold, m_nextFootHold) > maxDistanceWithNextFootHold)
+        if (m_move)
+            return;
+ 
+        if (Vector3.Distance(m_footPosition, GetFootHoldPosition()) > feetMaxDistanceWithNextStep)
+        {
+            CheckNextStepGround();
+            if (m_nextStepGrounded)
             {
+                m_nextFootHold = m_hitInfo.point;// + bodyController.direction * stepDistance;
+
                 m_move = true;
                 m_movePassTime = 0;
                 m_curFootHold = transform.position;
             }
-        }  
+        }
     }
 
     public void StopMove(bool check)
     {
+        // transform.position = m_hitInfo.point + offsetWithRoot;
         if (!check)
             return;
 
-        if (m_nextStepGrounded && m_move)
+        if (m_curFootHold != m_nextFootHold)
         {
-            m_nextFootHold = m_hitInfo.point;
-            // if (Vector3.Distance(m_curFootHold, m_nextFootHold) > maxDistanceWithNextFootHold)
-            // {
-                // m_move = true;
-                m_movePassTime = 0;
-                m_curFootHold = transform.position;
-            // }
+            m_nextFootHold = GetFootHoldPosition();
+            m_curFootHold = transform.position;
+            m_move = true;
+            m_movePassTime = 0;
         }  
     }
 
-    private void CheckGround()
+    private Vector3 GetFootHoldPosition()
     {
-        m_rootPosition = bodyRoot.position;
-        m_nextStepGrounded = Physics.Raycast(m_rootPosition + offsetWithRoot + Vector3.up * stepHeight, Vector3.down, out m_hitInfo, 10f);
+        return bodyController.root.TransformPoint(offsetWithRoot);
+    }
+
+    private void CheckNextStepGround()
+    {
+        // m_nextStepGrounded = Physics.Raycast(GetOffsetWithRoot() + Vector3.up * stepHeight + bodyController.direction * stepDistance, 
+        //     Vector3.down, 
+        //     out m_hitInfo, 
+        //     10f);
+
+        {
+            var result = Physics.SphereCast(GetFootHoldPosition() + bodyController.direction * stepDistance + Vector3.up * stepHeight,
+                nextStepCheckRadius, 
+                Vector3.down, 
+                out m_hitInfo,
+                stepHeight * 10f);
+
+            var orgin = GetFootHoldPosition() + bodyController.direction * stepDistance + Vector3.up * stepHeight;
+            Debug.DrawLine(orgin, orgin + Vector3.down * stepHeight * 10f, Color.blue);
+            if (result)
+            {
+                // Debug.Log($" CheckNextStepGround point {m_hitInfo.point} normal {m_hitInfo.normal} {Vector3.Angle(m_hitInfo.normal, Vector3.up)} ");
+                if (Vector3.Angle(m_hitInfo.normal, Vector3.up) <= stepAngle)
+                {
+                    m_nextStepGrounded = true;
+                    return;
+                }
+            }
+
+            m_nextStepGrounded = false;
+        }
+
+        // {
+        //     RaycastHit[] hitResult = Physics.SphereCastAll(GetOffsetWithRoot() + bodyController.direction * stepDistance + Vector3.up * stepHeight,
+        //         nextStepCheckRadius, 
+        //         Vector3.down, 
+        //         10f);
+
+        //     var orgin = GetOffsetWithRoot() + bodyController.direction * stepDistance + Vector3.up * stepHeight;
+        //     Debug.DrawLine(orgin, orgin + Vector3.down * stepHeight * 2, Color.blue);
+        //     Debug.Log($" CheckNextStepGround {hitResult.Length} ");
+        //     if (hitResult.Length > 0)
+        //     {
+        //         foreach (var item in hitResult)
+        //         {
+        //             // Debug.Log($" CheckNextStepGround Vector3.Angle(item.normal, Vector3.up) {Vector3.Angle(item.normal, Vector3.up)} ");
+        //             if (Vector3.Angle(item.normal, Vector3.up) <= stepAngle)
+        //             {
+        //                 m_hitInfo = item;
+        //                 m_nextStepGrounded = true;
+        //                 return;
+        //             }
+        //         }
+        //     }
+
+        //     m_nextStepGrounded = false;
+        // }
     }
 }
--- Client/Assets/Scripts/GamePlay/Anim/ProceduralMoveController.cs
+++ Client/Assets/Scripts/GamePlay/Anim/ProceduralMoveController.cs
@@ -4,17 +4,33 @@ using UnityEngine;
 
 public class ProceduralMoveController : MonoBehaviour
 {
+    public Transform root;
     public float moveSpeed;
     public float gravityValue = -9.81f;
     public float bodyHeigt = 1f;
     
-    public Vector3 direction;
+    public Transform followTarget;
+    public bool follow;
 
+    public Vector3 direction;
     public float feetMoveInterval;
     public ProceduralFootMotion[] feet;
+    public float feetDistanceWithBody;
+    public float feetMaxDistanceWithNextStep;
+    public Vector2 feetAngleOffset;
+
+
+    public float nextStepCheckRadius = 30;
+    public float stepAngle;
+    public float stepDistance;
+    public float stepHeight = 1f;
+    public float stepAnimHeight = 0.5f;
 
     private RaycastHit hitInfo;
+    private bool m_moving;
     private Vector3 m_position;
+    private Vector3 m_targetPosition;
+    private float m_feetHeightDiff;
 
     private float m_curMoveFeetPassTime;
     private int m_moveFeetIndex;
@@ -23,32 +39,147 @@ public class ProceduralMoveController : MonoBehaviour
     {
         DontDestroyOnLoad(gameObject);
         m_position = transform.position;
+        UpdateFeetPostion();
+    }
+
+    void UpdateFeetPostion()
+    {
+        List<ProceduralFootMotion> leftFeet = new List<ProceduralFootMotion>();
+        List<ProceduralFootMotion> rightFeet = new List<ProceduralFootMotion>();
+        foreach (var item in feet)
+        {
+            if (item.relationWithBody == ERelationWithBody.Left)
+                leftFeet.Add(item);
+            else
+                rightFeet.Add(item);
+        }
+
+        
+        var leftUnitAngle = (180 - feetAngleOffset.x - feetAngleOffset.y)/(leftFeet.Count - 1);
+        for (int i = 0; i < leftFeet.Count; i++)
+        {
+            var angle = feetAngleOffset.x + leftUnitAngle * i;
+            Debug.Log($"left angle {i} {leftFeet[i].name} {angle}");
+            if (angle < 90)
+            {
+                leftFeet[i].offsetWithRoot = new Vector3(Mathf.Sin(angle) * feetDistanceWithBody, 0, Mathf.Cos(angle) * feetDistanceWithBody);
+            }
+            else if(angle == 90)
+            {
+                leftFeet[i].offsetWithRoot = new Vector3(-feetDistanceWithBody, 0, 0);
+            }
+            else if (angle > 90)
+            {
+                leftFeet[i].offsetWithRoot = new Vector3(Mathf.Cos(angle - 90) * feetDistanceWithBody, 0, -Mathf.Sin(angle - 90) * feetDistanceWithBody);
+            }
+        }
+
+        var rightUnitAngle = (180 - feetAngleOffset.x - feetAngleOffset.y)/(rightFeet.Count - 1);
+        for (int i = 0; i < rightFeet.Count; i++)
+        {
+            var angle = feetAngleOffset.x + rightUnitAngle * i;
+            Debug.Log($"right angle {i} {rightFeet[i].name} {angle}");
+            if (angle < 90)
+            {
+                rightFeet[i].offsetWithRoot = new Vector3(-Mathf.Sin(angle) * feetDistanceWithBody, 0, Mathf.Cos(angle) * feetDistanceWithBody);
+            }
+            else if(angle == 90)
+            {
+                rightFeet[i].offsetWithRoot = new Vector3(feetDistanceWithBody, 0, 0);
+            }
+            else if (angle > 90)
+            {
+                rightFeet[i].offsetWithRoot = new Vector3(-Mathf.Cos(angle - 90) * feetDistanceWithBody, 0, -Mathf.Sin(angle - 90) * feetDistanceWithBody);
+            }
+        }
     }
 
     void Update()
     {
-        Move();
+        Debug.DrawLine(transform.position, transform.position + transform.forward *10 );
+        // UpdateFeetPostion();
+        if (follow && followTarget!=null)
+            FollowMove();
+        else    
+            HorizontalMove();
+
+        UpdateFeetIndex();
+        UpdateFeet();
+        UpdateHeight();
+        UpdateDirection();
     }
 
-    void Move()
+    void UpdateFeetIndex()
     {
-        Vector3 move = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
-        direction = move.normalized;
-              
-        m_position += move * Time.deltaTime * moveSpeed;
-        m_position.y = GetFeetHeight() + bodyHeigt;
-        transform.position = m_position;
-
         m_curMoveFeetPassTime += Time.deltaTime;
         if (m_curMoveFeetPassTime >= feetMoveInterval)
         {
             m_moveFeetIndex = (m_moveFeetIndex + 1) % feet.Length;
             m_curMoveFeetPassTime = 0;
         }
+    }
+
+    void FollowMove()
+    {
+        if (!GetFeetCanMove())
+            return;
+
+        m_targetPosition = followTarget.position;
+        m_targetPosition.y = m_position.y;
+        m_position = Vector3.MoveTowards(m_position, m_targetPosition, Time.deltaTime * moveSpeed);
+        transform.position = m_position;
+
+        m_moving = Vector3.Distance(m_targetPosition, m_position) > Time.deltaTime * moveSpeed;
+        if (m_moving)
+        {
+            direction = (m_targetPosition - m_position);
+            direction.y = 0;
+            direction = direction.normalized;
+        }
+    }
+    
+    void UpdateFeet()
+    {
+        if (m_moving)
+            for (int i = 0; i < feet.Length; i++)
+            {  
+                feet[i].TryMove(m_moveFeetIndex == i);
+            }
+        // else
+            // for (int i = 0; i < feet.Length; i++)
+            // {  
+            //     feet[i].StopMove(m_moveFeetIndex == i);
+            // }
+    }
+
+    void UpdateDirection()
+    {
+        if (!m_moving)
+            return;
+
+        //if (Vector3.Dot(direction, root.forward) < 0)
+        //    root.forward = (direction + Vector3.up * m_feetHeightDiff/2).normalized;
+        //else
+            root.forward = Vector3.RotateTowards(root.forward, (direction + Vector3.up * m_feetHeightDiff/2).normalized, Time.deltaTime * moveSpeed, Time.deltaTime * moveSpeed);
+    }
+
+    void HorizontalMove()
+    {   
+        Vector3 move = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        // move = transform.TransformDirection(move);
+        m_moving = move != Vector3.zero;
+        if (move == Vector3.zero)
+            return;
+
+        direction = move.normalized;
+        if (!GetFeetCanMove())
+            return;
+
+        m_position += move * Time.deltaTime * moveSpeed;
+        transform.position = m_position;
 
         if (move != Vector3.zero)
         {
-            // gameObject.transform.forward = move;
             for (int i = 0; i < feet.Length; i++)
             {  
                 feet[i].TryMove(m_moveFeetIndex == i);
@@ -63,14 +194,57 @@ public class ProceduralMoveController : MonoBehaviour
         }
     }
 
+    void UpdateHeight()
+    {
+        if (!m_moving)
+            return;
+
+        var height = GetFeetHeight() + bodyHeigt;
+        if (Mathf.Abs(m_position.y - height) <= Time.deltaTime * moveSpeed)
+        {
+            m_position.y = height;
+            return;
+        }
+        
+        var diff = (height - m_position.y);
+        m_position.y += diff * Time.deltaTime * moveSpeed; 
+    }
+    
     float GetFeetHeight()
     {
-        var maxHeight = Mathf.NegativeInfinity;
+        var sum = 0f;
+        // var maxHeight = Mathf.NegativeInfinity;
+        // var minHeight = Mathf.Infinity;
+        var frontMaxHeight = Mathf.NegativeInfinity;
+        var backMaxHeight = Mathf.NegativeInfinity;
         foreach (var item in feet)
-        {
-            maxHeight = Mathf.Max(item.height, maxHeight);
+        {   
+            sum += item.height;
+            // maxHeight = Mathf.Max(item.height, maxHeight);
+            // minHeight = Mathf.Min(item.height, minHeight);
+
+            if (root.TransformDirection(item.offsetWithRoot).z >= 0)
+                frontMaxHeight = Mathf.Max(item.height, frontMaxHeight);
+            else
+                backMaxHeight = Mathf.Max(item.height, backMaxHeight);
         }
 
-        return maxHeight;
+        m_feetHeightDiff = frontMaxHeight - backMaxHeight;
+        return sum/feet.Length;
+        // return maxHeight;
+    }
+
+    bool GetFeetCanMove()
+    {
+        foreach (var item in feet)
+        {   
+            if (!item.CheckCanMove())
+            {
+                // Debug.Log("GetFeetCanMove " + item.name);
+                return false;
+            }
+        }
+
+        return true;
     }
 }
